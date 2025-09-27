429920fc7213a1866357502421bcd664f817a784
jaroslawberek <berekjaroslaw@gmail.com>
2025-09-27 23:48:45 +0200

    objekty

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import PersonModule from "./js/objects/person.js";
+// import Handlebars from "https://esm.sh/handlebars";
 import { spliceTab, sliceTab, fileInfo, formTab, concatTab, sort1, sort2, filterTabWords, funparam, filterTab, fixName, WhaAlaDoing, countWord, generateID, printNumbers } from "./js/tablice.js";
-import { toDo } from "./js/todo.js";
+import { InitToDo } from "./js/todo.js";
 /* console.log("z app.js");
 const p = new PersonModule.Person(12, "Jurek", "Imadełko", "21");
 PersonModule.$("#link").on("fun");
@@ -32,4 +33,44 @@ generateID(15);
 //printNumbers(12);
 //WhaAlaDoing("mrzec"); */
 //dom1();
-toDo();
+
+// InitToDo();
+
+
+
+
+// const obj = {
+//     id: 1,
+//     name: "Janek",
+//     show: function show(a) {
+//         console.log(this);
+//     }
+// }
+
+// Object.defineProperty(obj, "name", {
+//     writable: false,
+//     enumerable: false
+// })
+
+// //obj.name = "Zofia"; //Cannot assign to read only property 'name' of object '#<Object>'
+// for (const key in obj) {
+//     if (!Object.hasOwn(obj, key)) continue; // nie wyswietli name
+//     console.log(`${key} : ${obj[key]}`);
+
+// }
+// console.log(Object.getOwnPropertyDescriptor(obj, "id"));  //jakie ustawienia ma włąsciwosc obiektu obj
+// console.log(Object.getOwnPropertyDescriptor(obj, "name"));
+// Object.preventExtensions(obj); //nie dodany nowych własciwosci ale mozemy usuwac własiwosci lub zmieniac mu ustawinia
+// console.log("Czy obiekt jest rozszerzalny - " + Object.isExtensible(obj)); //sprawdzamy rozszerzalnosc
+
+//Object.freeze(obj);
+// obj.id = 453;
+// obj.nowa = "nowy"; //Cannot add property nowa, object is not extensible
+// delete obj.id;          //Cannot delete property 'id' of #<Object>
+
+
+//console.log(Object.hasOwn(obj, "name") || "nie ma");
+
+
+/*********************************** */
+
--- js/todo.js
+++ js/todo.js
@@ -1,3 +1,157 @@
-export function toDo() {
-  console.log("test todo");
+import Handlebars from "https://esm.sh/handlebars";
+import { faker } from "/node_modules/@faker-js/faker/dist/locale/pl.js"
+
+const task_template = `<tr id="{{task_id}}" class="todo__task">
+                <th scope="row">{{pos}}</th>
+                <td class="todo__task-text">
+                 {{task_text}}
+                </td>
+                <td>{{task_create}}</td>
+                <td>{{deadline}}</td>
+                <td>
+                {{#switch priority}}
+                  {{#case "hight"}}
+                    <span class="todo__task-priority bg-danger">Wysoki</span>{{/case}}
+                  {{#case "middle"}}
+                    <span class="todo__task-priority bg-warning">Średni</span>{{/case}}
+                  {{#case "normal"}}
+                    <span class="todo__task-priority bg-info">Normalny</span>{{/case}}
+                {{/switch}}
+                </td>
+                <td class="todo__task-options">
+                  <a href="#" class="text-secondary close-task me-2"><i class="bi bi-x-circle"></i></a>
+                  <a href="#" class="text-primary edit-task me-2"><i class="bi bi-pencil-square"></i></a>
+                  <a href="#" class="text-danger delete-task"><i class="bi bi-trash"></i></a>
+                </td>
+              </tr>`;
+
+const todoTaskTemplate = Handlebars.compile(task_template);
+function getTask(pos) {
+  if (!pos) throw ("Nie podano pozycji");
+  return {
+    task_id: Date.now,
+    pos: pos,
+    task_text: faker.word.words({ count: { min: 5, max: 20 } }),
+    task_create: faker.date.between({ from: '2025-01-01', to: '2025-09-30' }).toISOString().split('T')[0], // '2026-05-16T02:22:53.002Z',
+    deadline: faker.date.between({ from: '2025-09-30', to: '2025-12-30' }).toLocaleDateString("pl-PL"),
+    priority: faker.helpers.arrayElement(['hight', 'middle', 'normal']) // 'dog',
+  }
+}
+
+// helpers.js
+function registerHelpers(Handlebars) {
+  // 🔸 Porównania
+  Handlebars.registerHelper("eq", (a, b) => a === b);
+  Handlebars.registerHelper("ne", (a, b) => a !== b);
+  Handlebars.registerHelper("gt", (a, b) => a > b);
+  Handlebars.registerHelper("lt", (a, b) => a < b);
+  Handlebars.registerHelper("gte", (a, b) => a >= b);
+  Handlebars.registerHelper("lte", (a, b) => a <= b);
+
+  // 🔸 Logika
+  Handlebars.registerHelper("and", (a, b) => a && b);
+  Handlebars.registerHelper("or", (a, b) => a || b);
+
+  // 🔸 Tekst
+  Handlebars.registerHelper("upper", str => String(str).toUpperCase());
+  Handlebars.registerHelper("lower", str => String(str).toLowerCase());
+  Handlebars.registerHelper("capitalize", str => {
+    if (!str) return "";
+    str = String(str);
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  });
+
+  // 🔸 JSON (debug)
+  Handlebars.registerHelper("json", context => JSON.stringify(context, null, 2));
+
+  // 🔸 Iteracja z indeksem
+  Handlebars.registerHelper("eachWithIndex", function (array, options) {
+    let result = "";
+    array.forEach((item, i) => {
+      result += options.fn({ ...item, index: i });
+    });
+    return result;
+  });
+
+  // 🔸 Daty
+  Handlebars.registerHelper("formatDate", function (dateStr, locale = "pl-PL") {
+    const d = new Date(dateStr);
+    return d.toLocaleDateString(locale);
+  });
+
+  // 🔸 Switch/Case
+  Handlebars.registerHelper("switch", function (value, options) {
+    this._switch_value_ = value;
+    const html = options.fn(this);
+    delete this._switch_value_;
+    return html;
+  });
+
+  Handlebars.registerHelper("case", function (value, options) {
+    if (value == this._switch_value_) {
+      return options.fn(this);
+    }
+  });
+}
+function renderTasks(tasksArray, taskTable) {
+  let taskHtml = "";
+  for (const task of tasksArray) {
+    taskHtml = todoTaskTemplate(task);
+    taskTable.innerHTML += taskHtml;
+
+  }
+}
+export function InitToDo() {
+  registerHelpers(Handlebars); // rejestracja wszystkich helperów
+  const tasksArray = [];
+  let pos = 1;
+  for (let i = 0; i < 10; i++)
+    tasksArray.push(getTask(pos++));
+
+  console.log(tasksArray);
+  document.addEventListener("DOMContentLoaded", (event) => {
+
+    const deleteTask = btn => {
+      btn.remove();
+
+    }
+    const closeTask = btn => console.log("closeTask: " + btn.closest(".todo__task").getAttribute("id"));
+    const editTask = btn => console.log("editTask: " + btn.closest(".todo__task").getAttribute("id"));
+
+    const taskAdd = document.querySelector('.nav-link[data-task-nav="add"]');
+    const todoTable = document.querySelector(".todo__table");
+
+    renderTasks(tasksArray, todoTable);
+
+    todoTable.addEventListener("click", function (e) {
+      e.stopPropagation();
+      const deleteBtn = e.target.closest(".delete-task");
+      const editBtn = e.target.closest(".edit-task");
+      const closeBtn = e.target.closest(".close-task");
+      if (deleteBtn && todoTable.contains(deleteBtn)) {
+        e.preventDefault(); // żeby # nie skakał do góry
+        deleteTask(deleteBtn.closest(".todo__task"));
+      }
+      else if (editBtn && todoTable.contains(editBtn)) {
+        e.preventDefault(); // żeby # nie skakał do góry
+        editTask(editBtn.closest(".todo__task"));
+      }
+      else if (closeBtn && todoTable.contains(closeBtn)) {
+        e.preventDefault(); // żeby # nie skakał do góry
+        closeTask(closeBtn.closest(".todo__task"));
+      }
+
+
+
+    });
+
+    taskAdd.addEventListener("click", function (e) {
+      const taskTable = document.querySelector(".todo__table tbody");
+      const task = getTask(taskTable.children.length + 1,);
+      const taskHtml = todoTaskTemplate(task);
+      taskTable.innerHTML += taskHtml;
+    });
+
+  });      //DOMContentLoaded
+
 }
